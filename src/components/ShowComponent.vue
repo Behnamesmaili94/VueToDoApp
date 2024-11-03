50901a83925f01ee7302cced4698d465013ba657
NothingProgrammer <Behnamesmaili94@gmail.com>
2024-11-03 14:38:56 +0330

    complate vueTodoApp

---
+++ src/components/ShowComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <slot name="header"></slot>
+  <slot name="body"></slot>
+  <slot name="footer"></slot>
+</template>
+
+<script>
+export default {
+  name:'ShowComponent',
+
+}
+</script>
+
+<style scoped>
+
+</style>
